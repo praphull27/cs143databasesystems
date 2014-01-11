a9e321b84bc8e9390f47739f30655ac0e2436ce2
Praphull Kumar <praphull@cs-210-137.cs.ucla.edu>
2014-01-10 20:06:04 -0800

    Creating the calculator Project Folder and moving calculator.php file to it

---
+++ calculator/calculator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head><title>Calculator for CS143 - Database Systems Course</title></head>
+<body>
+	<h1>Calculator</h1>
+	(Ver 0.1 01/15/2014 by Praphull Kumar)<br />
+	Type an expression in the following box (e.g., 10.5+20*3/25).
+	<p>
+		<form method="GET">
+			<input type="text" name="expr"><input type="submit" value="Calculate">
+		</form>
+	</p>
+	<ul>
+		<li>Only numbers and +, -, * and / operators are allowed in the expression.</li>
+		<li>The evaluation follows the standard operator precedence.</li>
+		<li>Both integers and floating point numbers are allowed.</li>
+		<li>The calculator does not support parentheses.</li>
+		<li></li>
+	</ul>
+	<!-- <h2>Result</h2>
+	1+1 = 2 -->
+</body>
+</html>
