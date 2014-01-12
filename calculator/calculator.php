92c6ae43fbba28f404fbf14055af89cca6125342
Praphull Kumar <praphull@cs-210-137.cs.ucla.edu>
2014-01-11 17:37:48 -0800

    Updated calculator.php to clean the code

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -15,72 +15,71 @@
 		<li>The evaluation follows the standard operator precedence.</li>
 		<li>Both integers and floating point numbers are allowed.</li>
 		<li>The calculator does not support parentheses.</li>
-		<!--<li>Whitespace entered in the expression will be removed.</li>-->
 	</ul>
 	<?php 
 		//Checking if user has provided an expression.
-		if($_GET[expression] != '') {
+	if($_GET[expression] != '') {
 
 			//Initializing a varaible named 'expression' with the expression provided by the user.
-			$expression = $_GET[expression];
+		$expression = $_GET[expression];
 
-			$errstring = '';
-			//error handler function
-			function customError($errno, $errstr) {
-				global $errstring;
-				$errstring = $errstr;
-			}
-			//set error handler
-			set_error_handler("customError", E_ALL);
+			//Setting Error Handler function for Divison by Zero
+		$errstring = '';
+		function customError($errno, $errstr) {
+			global $errstring;
+			$errstring = $errstr;
+		}
+		set_error_handler("customError", E_ALL);
 
-			//$expression = preg_replace("/\s+/", "", $expression);
+			//Checking for allowed chars in the expression string i.e. numbers, whitespace, +, -, *, / and Decimal point for floating numbers.
+		if(!preg_match("/^[0-9\+\-\*\/ \.]+$/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(!preg_match("/^[0-9\+\-\*\/ \.]+$/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+			//Checking for invalid input - Whitespace between digits e.g "1 9"
+		if(preg_match("/[0-9]\s+[0-9]/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(preg_match("/[0-9]\s+[0-9]/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+			//Checking for invalid input - Two operators together. Exceptions: (+-, -+) when between two numbers, (*+, *-, /+, /-) when followd by a number.
+		if(preg_match("/[\+\-\*\/][\+\-\*\/]/", $expression) && !preg_match("/[0-9]\s*[\+\-][\+\-][0-9]/", $expression) && !preg_match("/[\*\/][\+\-][0-9]/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(preg_match("/[\+\-\*\/][\+\-\*\/]/", $expression) && !preg_match("/[0-9]\s*[\+\-][\+\-][0-9]/", $expression) && !preg_match("/[\*\/][\+\-][0-9]/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+		if(preg_match("/\+\+/", $expression) || preg_match("/\-\-/", $expression) || preg_match("/[\*\/]\s*[\*\/]/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(preg_match("/\+\+/", $expression) || preg_match("/\-\-/", $expression) || preg_match("/[\*\/]\s*[\*\/]/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+		if(preg_match("/^[\*\/]/", $expression) || preg_match("/[\+\-\*\/]$/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(preg_match("/^[\*\/]/", $expression) || preg_match("/[\+\-\*\/]$/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
-			
-			if(preg_match("/\s[\+\-\*\/]\s+[\+\-\*\/]\s*/", $expression) && !preg_match("/[0-9]\s+[\+\-\*\/]\s+[\+\-][0-9]/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+		if(preg_match("/\s[\+\-\*\/]\s+[\+\-\*\/]\s*/", $expression) && !preg_match("/[0-9]\s+[\+\-\*\/]\s+[\+\-][0-9]/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
-			if(preg_match("/\.\./", $expression) || preg_match("/\.[0-9]\./", $expression) || preg_match("/\.\s+[0-9]/", $expression)) {
-				$errstring="Invalid Input Expression $expression";
-			}
+		if(preg_match("/\.\./", $expression) || preg_match("/\.[0-9]\./", $expression) || preg_match("/\.\s+[0-9]/", $expression)) {
+			$errstring="Invalid Input Expression";
+		}
 
 			//Evaluating the expression using PHP's eval() method.
-			if ($errstring == '') {
-				eval("\$result = $expression;");
-			}
-	
-			//Echoing the Result
-			if ($errstring == ''){
-	?> 
-				<h2>Result</h2>
-	<?php 
-				echo "$expression = $result";
-			} else {
-	?> 
-				<h2>Error</h2>
-	<?php 
-				echo "$errstring";
-			}
+		if ($errstring == '') {
+			eval("\$result = $expression;");
 		}
+
+			//Echoing the Result
+		if ($errstring == '') {
+	?> 
+	<h2>Result</h2>
+	<?php 
+			echo "$expression = $result";
+		} else {
+	?> 
+	<h2>Error</h2>
+	<?php 
+			echo "$errstring $expression";
+		}
+	}
 	?>
 </body>
 </html>
