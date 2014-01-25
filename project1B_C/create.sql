634664176bbdb9770380fb5a83cc5bfef1bcfb38
Praphull Kumar <praphull@cs-210-71.cs.ucla.edu>
2014-01-25 15:13:41 -0800

    create.sql and load.sql

---
+++ project1B_C/create.sql
@@ -0,0 +1,85 @@
+USE TEST;
+
+DROP TABLE IF EXISTS MovieGenre;
+DROP TABLE IF EXISTS MovieDirector;
+DROP TABLE IF EXISTS MovieActor;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS MaxPersonID;
+DROP TABLE IF EXISTS MaxMovieID;
+DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Actor;
+DROP TABLE IF EXISTS Director;
+
+CREATE TABLE `Movie` (
+	`id` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Movie ID',
+	`title` VARCHAR(100) NOT NULL COLLATE utf8_general_ci COMMENT 'Movie Title',
+	`year` INT COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Release Year',
+	`rating` VARCHAR(10) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'MPAA Rating',
+	`company` VARCHAR(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Production Company',
+	PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `Actor` (
+	`id` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Actor ID',
+	`last` VARCHAR(20) NOT NULL COLLATE utf8_general_ci COMMENT 'Last name',
+	`first` VARCHAR(20) NOT NULL COLLATE utf8_general_ci COMMENT 'First name',
+	`sex` VARCHAR(6) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Sex of the actor',
+	`dob` DATE NOT NULL COMMENT 'Date of Birth',
+	`dod` DATE DEFAULT NULL COMMENT 'Date of Death',
+	PRIMARY KEY (id),
+	CHECK (dob < dod )
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `Director` (
+	`id` INT NOT NULL  COLLATE utf8_general_ci COMMENT 'Director ID',
+	`last` VARCHAR(20) NOT NULL  COLLATE utf8_general_ci COMMENT 'Last name',
+	`first` VARCHAR(20) NOT NULL  COLLATE utf8_general_ci COMMENT 'First name',
+	`dob` DATE NOT NULL COMMENT 'Date of Birth',
+	`dod` DATE DEFAULT NULL COMMENT 'Date of Death',
+	PRIMARY KEY (id),
+	CHECK (dob < dod )
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `MovieGenre` (
+	`mid` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Movie ID',
+	`genre` VARCHAR(20) NOT NULL COLLATE utf8_general_ci COMMENT 'Movie genre',
+	FOREIGN KEY (mid) REFERENCES Movie( id ),
+	UNIQUE(mid,genre)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `MovieDirector` (
+	`mid` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Movie ID',
+	`did` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Director ID',
+	FOREIGN KEY (mid) REFERENCES Movie( id ),
+	FOREIGN KEY (did) REFERENCES Director( id ),
+	UNIQUE(mid,did)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `MovieActor` (
+	`mid` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Movie ID',
+	`aid` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Actor ID',
+	`role` VARCHAR(50) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Actor role in movie',
+	FOREIGN KEY (mid) REFERENCES Movie( id ),
+	FOREIGN KEY (aid) REFERENCES Actor( id ),
+	UNIQUE(mid,aid,role)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `Review` (
+	`name` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Reviewer name',
+	`time` TIMESTAMP NOT NULL COMMENT 'Review time',
+	`mid` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Movie ID',
+	`rating` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Review rating',
+	`comment` VARCHAR(500) COLLATE utf8_general_ci DEFAULT NULL COMMENT 'Reviewer Comment',
+	PRIMARY KEY (name, mid),
+	FOREIGN KEY (mid) REFERENCES Movie( id ),
+	CHECK ( rating <= 5)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci; 
+
+Create Table `MaxPersonID` (
+	`id` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Max ID assigned to all persons'
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+Create Table `MaxMovieID` (
+	`id` INT NOT NULL COLLATE utf8_general_ci COMMENT 'Max ID assigned to all movies'
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
---
+++ project1B_C/load.sql
@@ -0,0 +1,55 @@
+USE TEST;
+
+-- Loading Data into Tables from the files.
+
+-- Loading Movie Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/movie.del" INTO TABLE Movie
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading Actor Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/actor1.del" INTO TABLE Actor
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/actor2.del" INTO TABLE Actor
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/actor3.del" INTO TABLE Actor
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading Director Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/director.del" INTO TABLE Director
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading MovieGenre Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/moviegenre.del" INTO TABLE MovieGenre
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading MovieDirector Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/moviedirector.del" INTO TABLE MovieDirector
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading MovieActor Tables
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/movieactor1.del" INTO TABLE MovieActor
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+LOAD DATA LOCAL INFILE "/home/cs143/data/movieactor2.del" INTO TABLE MovieActor
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+
+-- Loading MaxPersonID
+INSERT INTO MaxPersonID VALUES (69000);
+
+
+-- Loading MaxMovieID
+INSERT INTO MaxMovieID VALUES (4750);
